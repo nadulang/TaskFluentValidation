ad64a38231188470158f8e225d5ebecb6fe0da24
Training Gigaming <gigaming@ggm02s-iMac.local>
2020-03-04 11:44:49 +0700

    lima

--- Validator/MerchantValidator.cs
+++ Validator/MerchantValidator.cs
@@ -11,7 +11,7 @@ namespace TaskMediatrFluentValidation.Validator
         {
             RuleFor(x => x.data.attributes.name).NotEmpty().WithMessage("name can't be empty");
             RuleFor(x => x.data.attributes.address).NotEmpty().WithMessage("address can't be empty");
-            RuleFor(x => x.data.attributes.rating).ExclusiveBetween(1, 5).WithMessage("rating is bettween 1-5");
+            RuleFor(x => x.data.attributes.rating).ExclusiveBetween(0, 6).WithMessage("rating is bettween 1-5");
         }
     }
 }
