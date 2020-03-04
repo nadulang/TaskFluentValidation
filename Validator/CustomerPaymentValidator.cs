893aa4ce5530b3f039937f733abbaff1ddd6cede
Training Gigaming <gigaming@ggm02s-iMac.local>
2020-03-04 12:04:21 +0700

    enam

--- Validator/CustomerPaymentValidator.cs
+++ Validator/CustomerPaymentValidator.cs
@@ -12,7 +12,7 @@ namespace TaskMediatrFluentValidation.Validator
             RuleFor(x => x.data.attributes.name_on_card).NotEmpty().WithMessage("name_on_card can't be empty");
             RuleFor(x => x.data.attributes.name_on_card).MaximumLength(50).WithMessage("max name_on_card length is 50");
             RuleFor(x => x.data.attributes.exp_month).NotEmpty().WithMessage("exp_month can't be empty");
-            RuleFor(x => Convert.ToInt32(x.data.attributes.exp_month)).ExclusiveBetween(1, 12).WithMessage("exp_month is between 1-12");
+            RuleFor(x => Convert.ToInt32(x.data.attributes.exp_month)).ExclusiveBetween(0, 13).WithMessage("exp_month is between 1-12");
             RuleFor(x => x.data.attributes.exp_year).NotEmpty().WithMessage("exp_year can't be empty");
             RuleFor(x => x.data.attributes.credit_card_number).CreditCard().WithMessage("credit_card_number must be type of credit card number");
         }
