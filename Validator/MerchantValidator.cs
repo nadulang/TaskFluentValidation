ad0fb9df9b38e53e640fbc52dc053daa9cdc3889
Training Gigaming <gigaming@ggm02s-iMac.local>
2020-03-04 11:38:29 +0700

    tiga

--- Validator/MerchantValidator.cs
+++ Validator/MerchantValidator.cs
@@ -10,8 +10,8 @@ namespace TaskMediatrFluentValidation.Validator
         public MerchantValidator()
         {
             RuleFor(x => x.data.attributes.name).NotEmpty().WithMessage("name can't be empty");
-            RuleFor(x => x.data.attributes.address).NotEmpty().WithMessage("max name_on_card length is 50");
-            RuleFor(x => x.data.attributes.rating).ExclusiveBetween(1, 5).WithMessage("exp_month is bettween 1-5");
+            RuleFor(x => x.data.attributes.address).NotEmpty().WithMessage("max address length is 50");
+            RuleFor(x => x.data.attributes.rating).ExclusiveBetween(1, 5).WithMessage("rating is bettween 1-5");
         }
     }
 }
